f6d04c8f467f20f5560bedcad4198abb12c16aee
matas234 <160629386+matas234@users.noreply.github.com>
2024-10-26 08:55:24 +0100

    added eye recognition

--- test.py
+++ test.py
@@ -14,6 +14,10 @@ class String:
         self.nature = 0
         self.liberties = np.zeros(total_length, dtype=int)
 
+        self.eyes = []
+        self.special_eyes = []
+        self.eye_likes = []
+
 
     def addStone(self, idx: int) -> None:
         self.stones[idx] = self.nature
@@ -21,6 +25,15 @@ class String:
 
     def addLiberty(self, idx: int) -> None:
         self.liberties[idx] = self.nature
+
+    def addEye(self, idx: int) -> None:
+        self.eyes.append(idx)
+
+    def addSpecialEye(self, idx: int) -> None:
+        self.special_eyes.append(idx)
+
+    def addEyeLike(self, idx: int) -> None:
+        self.eye_likes.append(idx)
 
 
     def generateConnectinos(self) -> None:
@@ -63,6 +76,7 @@ class Score:
         self.board = board.copy()
 
         self._cardinals_cache = [self.__computeCardinals(i) for i in range(self.total_length)]
+        self._neighbor_cache = [self._computeNeighbors(i) for i in range(self.total_length)]
 
         self.debugger = Debugger(self)
         self.string_manager = StringManager(self)
@@ -70,22 +84,32 @@ class Score:
 
     
     def __computeCardinals(self, indx: int) -> List[int]:
-        out = []
+        cardinals = []
         
         for offset in [self.row_length, -self.row_length, 1, -1]:
-            neigh = indx + offset
-            if (0 <= neigh < self.total_length and                       # in bounds of the board
+            cardinal = indx + offset
+            if (0 <= cardinal < self.total_length and                       # in bounds of the board
                 not (offset == -1 and indx % self.row_length == 0) and   # 
                 not (offset == 1 and (indx + 1) % self.row_length == 0)  # handling edges
             ):
-                out.append(neigh)
+                cardinals.append(cardinal)
 
-        return out 
+        return cardinals 
     
+    def _computeNeighbors(self, idx: int) -> List[int]:
+        neighbors = []
+        rl = self.row_length
+        
+        for offset in [rl, -rl, 1, -1, rl+1, rl-1, -rl+1, -rl-1]:
+            neigh = idx + offset
+            if (0 <= neigh < self.total_length and                       # in bounds of the board
+                not ((offset - rl) % rl == 18 and idx % rl == 0) and   # 
+                not ((offset + rl) % rl == 1 and (idx + 1) % rl == 0)  # handling edges
+            ):
+                neighbors.append(neigh)
 
+        return neighbors 
     
-    
-
 
 
 
@@ -222,25 +246,73 @@ class StringManager:
             self.groups.append(current_group)    
     
 
+    def findEyes(self) -> None:
+        for idx in range(len(self.strings)):
+            string = self.strings[idx]
+
+            for lib_idx in np.nonzero(string.liberties)[0]:
+                cardinals = self.score._cardinals_cache[lib_idx]
+                neighbors = self.score._neighbor_cache[lib_idx]                
+                
+                same_string_cardinals = sum(1 for c in cardinals if string.stones[c])
+
+                friendly_neighbors = sum(1 for n in neighbors if (self.score.board[n] == string.nature))
+                friendly_cardinals = sum(1 for n in cardinals if (self.score.board[n] == string.nature))
+
+                enemy_neighbors = sum(1 for n in neighbors if (self.score.board[n] == -string.nature))
+                enemy_cardinals = sum(1 for n in cardinals if (self.score.board[n] == -string.nature))
+                enemy_corners = enemy_neighbors - enemy_cardinals
+
+                empty_neighbors = sum(1 for n in neighbors if (self.score.board[n] == 0))    
+                
+
+                if same_string_cardinals == len(cardinals):                                                    # think about edges IMPORTANT
+                    string.addEye(lib_idx)
+
+                elif enemy_neighbors == 0 and friendly_neighbors >= 6 and friendly_cardinals == 4:
+                    string.addEye(lib_idx)
+
+
+                elif friendly_neighbors == 7 and enemy_neighbors == 0:
+                    string.addEye(lib_idx)
+
+                elif friendly_neighbors >= 6 and enemy_corners <= 1:
+                    string.addSpecialEye(lib_idx)
+
+                elif friendly_neighbors >= 5 and enemy_neighbors == 0:
+                    string.addEyeLike(lib_idx)
+                
+
+
+
+
+
+
+
+
+
 class Debugger:
     def __init__(self, score_instance: Score) -> None:
         self.score = score_instance
 
     def printHeatMap(self) -> None:
-            plt.figure(figsize=(8, 6))
-            terr = (self.score.bouzy.intensity * self.score.bouzy.nature)
-            map = np.where(self.score.board == 0, terr, board*64)
-            sns.heatmap(np.reshape(map, (self.score.row_length, self.score.row_length)), annot=True, cmap="coolwarm", cbar=True, center=0)
-            plt.savefig("assets/heatmap.png", dpi=300, bbox_inches='tight')
+        plt.figure(figsize=(8, 6))
+        terr = (self.score.bouzy.intensity * self.score.bouzy.nature)
+        map = np.where(self.score.board == 0, terr, board*64)
+        sns.heatmap(np.reshape(map, (self.score.row_length, self.score.row_length)), annot=True, cmap="coolwarm", cbar=True, center=0)
+        plt.savefig("assets/heatmap.png", dpi=300, bbox_inches='tight')
 
 
     def printStrings(self) -> None:
         plt.figure(figsize=(8, 6))
         to_plot = np.zeros(self.score.total_length, dtype=int)
+        
         for idx, string in enumerate(self.score.string_manager.strings):
             stones = string.stones * (idx + 1)
-
             to_plot += stones 
+            print(string.eyes)
+            for eye in string.eyes:
+                to_plot[eye] = string.nature * 100
 
         sns.heatmap(np.reshape(to_plot, (self.score.row_length, self.score.row_length)), annot=True, cmap="binary", cbar=True, center=0)
         plt.savefig("assets/hstrings.png", dpi=300, bbox_inches='tight')
@@ -258,17 +330,53 @@ class Debugger:
         plt.savefig("assets/hgroups.png", dpi=300, bbox_inches='tight')       
 
 
+    def printStringsText(self):
+        rl = self.score.row_length
+        with open("assets/out.txt", "w", encoding="utf-8") as f:
+            for i in range(len(self.score.string_manager.strings)):
+                string = self.score.string_manager.strings[i]
 
-board = loadGame("games/game1.sgf")
+                out_string = [["⚫"]*rl for _ in range(rl)]
+                out_con = [["⚫"]*rl for _ in range(rl)]
+                out_libs = [["⚫"]*rl for _ in range(rl)]
+
+                for idx in np.nonzero(string.stones)[0]:
+                    out_string[idx // rl][idx % rl] = "⚪"
+
+
+                for idx in np.nonzero(string.liberties)[0]:
+                    out_string[idx // rl][idx % rl] = "🟢"
+
+                for eye in string.eyes:
+                    out_string[eye // rl][eye % rl] = "🔵"
+
+                for s_eye in string.special_eyes:
+                    out_string[s_eye // rl][s_eye % rl] = "🟠"        
+
+                for eye_like in string.eye_likes:
+                    out_string[eye_like // rl][eye_like % rl] = "🟡"  
+
+
+
+                for line_index in range(rl):
+                    f.write(f"{''.join(out_string[line_index])}  {''.join(out_con[line_index])}  {''.join(out_libs[line_index])} \n")
+                f.write("\n\n")
+
+
+
+
+board = loadGame("games/eyes.sgf")
 potential = Score(board = board, size = int(np.sqrt(board.size)))
 
 
 
 s = time.time()
-
+potential = Score(board = board, size = int(np.sqrt(board.size)))
 potential.bouzy.bouzy(8, 21)
 potential.string_manager.findStrings()
 potential.string_manager.findGroups()
+potential.string_manager.findEyes()
+
 
 e = time.time()
 print(f"Took {e-s} seconds")
@@ -276,4 +384,4 @@ print(f"Took {e-s} seconds")
 
 potential.debugger.printGroups()
 potential.debugger.printHeatMap()
-potential.debugger.printStrings()
+potential.debugger.printStringsText()
